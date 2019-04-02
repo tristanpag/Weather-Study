3d2d4b31b771f8991c7cf7d79f68f8915b296d0d
Tristan <tristanpag@gmail.com>
2019-04-02 12:03:25 -0400

    Initial Commit

---
+++ Weather Study/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Weather Study
+//
+//  Created by Tristan Pageau on 2019-04-02.
+//  Copyright © 2019 Poly MTL. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
